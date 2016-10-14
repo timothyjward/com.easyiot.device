df6999f1cb741a4c55b0ce217169ec912afc6741
Tim Ward <timothyjward@apache.org>
2016-10-14 08:08:17 +0100

    Move to static optional references

--- com.easyiot.LT100H.device.provider/src/com/easyiot/LT100H/device/provider/LT100HDeviceImpl.java
+++ com.easyiot.LT100H.device.provider/src/com/easyiot/LT100H/device/provider/LT100HDeviceImpl.java
@@ -1,9 +1,13 @@
 package com.easyiot.LT100H.device.provider;
 
+import static org.osgi.service.component.annotations.ReferenceCardinality.OPTIONAL;
+import static org.osgi.service.component.annotations.ReferencePolicyOption.GREEDY;
+
 import java.util.concurrent.atomic.AtomicReference;
 
 import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Deactivate;
 import org.osgi.service.component.annotations.Reference;
 import org.osgi.service.component.annotations.ReferenceCardinality;
 import org.osgi.service.component.annotations.ReferencePolicy;
@@ -33,26 +37,17 @@ public class LT100HDeviceImpl implements Device {
 
 	@Reference
 	private DTOs dtoConverter;
-
+	
+	@Reference(name = "auslorawsProtocolReference", cardinality = OPTIONAL, policyOption = GREEDY)
+	private AusloraWebsocketProtocol auslorawsClient;
+	
+	@Reference(name = "mqttProtocolReference", cardinality = OPTIONAL, policyOption = GREEDY)
+	private TtnMqttProtocol ttnMqttClient;
+	
 	private AusloraWebsocketListener callback = (metadata) -> {
 		LT100HSensorDataDTO sensorData = myAusloraDataConverter.convert(metadata, dtoConverter);
 		lastKnownData.set(sensorData);
 	};
-
-	// bind ausloraProtocol
-	@Reference(name = "auslorawsProtocolReference", cardinality = ReferenceCardinality.OPTIONAL, policy = ReferencePolicy.DYNAMIC, unbind = "unSetAusloraWebSocket")
-	public void setAusloraWebSocket(AusloraWebsocketProtocol auslorawsClient) {
-		String deviceChannel = String.format("app?id=%s&token=%s", deviceConfiguration.applicationId(),
-				deviceConfiguration.securityToken());
-		auslorawsClient.connect(deviceChannel, deviceConfiguration.deviceEUI(), callback);
-	}
-
-	// unbind auslora client
-	public void unSetAusloraWebSocket(AusloraWebsocketProtocol auslorawsClient) {
-		String deviceChannel = String.format("app?id=%s&token=%s", deviceConfiguration.applicationId(),
-				deviceConfiguration.securityToken());
-		auslorawsClient.disconnect(deviceChannel, deviceConfiguration.deviceEUI(), callback);
-	}
 
 	// lambda that is subscribed to MQTT
 	private TtnMqttMessageListener subsMethod = (metadata) -> {
@@ -60,20 +55,33 @@ public class LT100HDeviceImpl implements Device {
 		lastKnownData.set(newData);
 	};
 
-	// Bind mqttClient
-	@Reference(name = "mqttProtocolReference", cardinality = ReferenceCardinality.OPTIONAL, policy = ReferencePolicy.DYNAMIC, unbind = "unMqttClient")
-	public void setMqttClient(TtnMqttProtocol ttnMqttClient) {
-		ttnMqttClient.subscribe(deviceConfiguration.subscriptionChannel(), subsMethod);
-	}
-
-	// Unbind mqttclient
-	public void unMqttClient(TtnMqttProtocol ttnMqttClient) {
-		ttnMqttClient.unsubscribe(deviceConfiguration.subscriptionChannel());
-	}
-
 	@Activate
 	public void activate(LT100HDeviceConfiguration deviceConfiguration) {
 		this.deviceConfiguration = deviceConfiguration;
+		
+		if(ttnMqttClient != null) {
+			ttnMqttClient.subscribe(deviceConfiguration.subscriptionChannel(), subsMethod);
+		}
+		
+		if(auslorawsClient != null) {
+			String deviceChannel = String.format("app?id=%s&token=%s", deviceConfiguration.applicationId(),
+					deviceConfiguration.securityToken());
+			auslorawsClient.connect(deviceChannel, deviceConfiguration.deviceEUI(), callback);
+		}
+	}
+	
+	@Deactivate
+	public void deactivate() {
+		
+		if(ttnMqttClient != null) {
+			ttnMqttClient.unsubscribe(deviceConfiguration.subscriptionChannel());
+		}
+		
+		if(auslorawsClient != null) {
+			String deviceChannel = String.format("app?id=%s&token=%s", deviceConfiguration.applicationId(),
+					deviceConfiguration.securityToken());
+			auslorawsClient.disconnect(deviceChannel, deviceConfiguration.deviceEUI(), callback);
+		}
 	}
 
 	@GetMethod
